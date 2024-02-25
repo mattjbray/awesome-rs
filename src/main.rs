6b7ff7ee7bc91ed030ecae035fdb45bece35a3c8
Matt Bray <mattjbray@gmail.com>
2024-02-25 13:30:02 +0000

    refactor: impl WindowState

--- src/main.rs
+++ src/main.rs
@@ -24,44 +24,55 @@ impl WindowState {
             mouse_offset,
         }
     }
+
+
+    fn from_mouse_location(system_wide_element: &AXUIElement) -> Option<Self> {
+        let mouse_location = CGEventSource::new(CGEventSourceStateID::CombinedSessionState)
+            .and_then(CGEvent::new)
+            .map(|e| e.location())
+            .unwrap();
+
+        let element = system_wide_element
+            .element_at_position(mouse_location.x as f32, mouse_location.y as f32)
+            .unwrap();
+
+        element
+            .window()
+            .map(|window| {
+                let window_pos: CGPoint = window.position().unwrap().get_value().unwrap();
+                let mouse_offset = CGPoint::new(
+                    mouse_location.x - window_pos.x,
+                    mouse_location.y - window_pos.y,
+                );
+                Self::new(window, mouse_offset)
+            })
+            .ok()
+    }
+
+    fn position_around(&self, point: &CGPoint) {
+        let x = point.x - self.mouse_offset.x;
+        let y = point.y - self.mouse_offset.y;
+
+        let position = CGPoint::new(x, y);
+
+        self.window
+            .set_attribute(
+                &AXAttribute::position(),
+                AXValue::from_CGPoint(position).unwrap(),
+            )
+            .unwrap();
+    }
+
+    fn activate(&self) {
+        let pid = self.window.pid().unwrap();
+        unsafe {
+            let app = NSRunningApplication::runningApplicationWithProcessIdentifier(nil, pid);
+            app.activateWithOptions_(NSApplicationActivationOptions::NSApplicationActivateAllWindows);
+        }
+        self.window.set_main(true).unwrap();
+    }
 }
 
-fn get_window_under_mouse(system_wide_element: &AXUIElement) -> Option<WindowState> {
-    let mouse_location = CGEventSource::new(CGEventSourceStateID::CombinedSessionState)
-        .and_then(CGEvent::new)
-        .map(|e| e.location())
-        .unwrap();
-
-    let element = system_wide_element
-        .element_at_position(mouse_location.x as f32, mouse_location.y as f32)
-        .unwrap();
-
-    element
-        .window()
-        .map(|window| {
-            let window_pos: CGPoint = window.position().unwrap().get_value().unwrap();
-            let mouse_offset = CGPoint::new(
-                mouse_location.x - window_pos.x,
-                mouse_location.y - window_pos.y,
-            );
-            WindowState::new(window, mouse_offset)
-        })
-        .ok()
-}
-
-fn position_window_around(s: &WindowState, point: &CGPoint) {
-    let x = point.x - s.mouse_offset.x;
-    let y = point.y - s.mouse_offset.y;
-
-    let position = CGPoint::new(x, y);
-
-    s.window
-        .set_attribute(
-            &AXAttribute::position(),
-            AXValue::from_CGPoint(position).unwrap(),
-        )
-        .unwrap();
-}
 
 fn main() {
     let system_wide_element = AXUIElement::system_wide();
@@ -78,21 +89,16 @@ fn main() {
             |_, event_type, event| {
                 match event_type {
                     MouseMoved => match state.borrow().as_ref() {
-                        Some(state) => position_window_around(&state, &event.location()),
+                        Some(state) => state.position_around(&event.location()),
                         None => (),
                     },
 
                     FlagsChanged => {
                         let mut s = state.borrow_mut();
                         if event.get_flags().contains(CGEventFlags::CGEventFlagCommand) {
-                            *s = get_window_under_mouse(&system_wide_element);
+                            *s = WindowState::from_mouse_location(&system_wide_element);
                             if let Some(window_state) = s.as_ref() {
-                                let pid = window_state.window.pid().unwrap();
-                                unsafe {
-                                    let app = NSRunningApplication::runningApplicationWithProcessIdentifier(nil, pid);
-                                    app.activateWithOptions_(NSApplicationActivationOptions::NSApplicationActivateAllWindows);
-                                }
-                                window_state.window.set_main(true).unwrap();
+                                window_state.activate()
                             }
                         } else {
                             *s = None;
