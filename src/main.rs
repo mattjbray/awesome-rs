4db15fd029cfb51f9c9f6c8a629b2b60a61711a7
Matt Bray <mattjbray@gmail.com>
2024-03-13 11:11:07 +0000

    refactor: struct Window

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,11 @@ use core_graphics::geometry::CGPoint;
 use core_graphics::window::{kCGWindowLayer, kCGWindowOwnerPID};
 
 #[derive(Debug)]
+struct Window(AXUIElement);
+
+#[derive(Debug)]
 struct WindowState {
-    window: AXUIElement,
+    window: Window,
     mouse_offset: CGPoint,
 }
 
@@ -58,22 +61,19 @@ fn get_application(element: &AXUIElement) -> Result<AXUIElement, accessibility::
     }
 }
 
-impl WindowState {
-    fn new(window: AXUIElement, mouse_offset: CGPoint) -> Self {
-        Self {
-            window,
-            mouse_offset,
-        }
-    }
+fn get_mouse_location() -> Result<CGPoint, ()> {
+    CGEventSource::new(CGEventSourceStateID::CombinedSessionState)
+        .and_then(CGEvent::new)
+        .map(|e| e.location())
+}
 
-    fn from_mouse_location(system_wide_element: &AXUIElement) -> Option<Self> {
-        let mouse_location = CGEventSource::new(CGEventSourceStateID::CombinedSessionState)
-            .and_then(CGEvent::new)
-            .map(|e| e.location())
-            .unwrap();
-
+impl Window {
+    fn at_point(
+        system_wide_element: &AXUIElement,
+        point: &CGPoint,
+    ) -> Result<Self, accessibility::Error> {
         let element = system_wide_element
-            .element_at_position(mouse_location.x as f32, mouse_location.y as f32)
+            .element_at_position(point.x as f32, point.y as f32)
             .unwrap();
 
         let element_is_window = match element.role() {
@@ -85,11 +85,52 @@ impl WindowState {
             Ok(element)
         } else {
             element.window()
-        };
+        }?;
+
+        Ok(Self(window))
+    }
+
+    fn get_position(&self) -> Result<CGPoint, accessibility::Error> {
+        let value = self.0.position()?;
+        value.get_value()
+    }
+
+    fn set_position(&self, x: f64, y: f64) -> Result<(), accessibility::Error> {
+        self.0.set_attribute(
+            &AXAttribute::position(),
+            AXValue::from_CGPoint(CGPoint::new(x, y)).unwrap(),
+        )
+    }
+
+    fn set_size(&self, w: f64, h: f64) -> Result<(), accessibility::Error> {
+        let size = CGSize::new(w, h);
+        self.0
+            .set_attribute(&AXAttribute::size(), AXValue::from_CGSize(size).unwrap())
+    }
+
+    /// Bring this window's application to front, and set this window as main.
+    fn activate(&self) -> Result<(), accessibility::Error> {
+        let app = get_application(&self.0)?;
+        app.set_attribute(&AXAttribute::frontmost(), true)?;
+        self.0.set_main(true)
+    }
+}
+
+impl WindowState {
+    fn new(window: Window, mouse_offset: CGPoint) -> Self {
+        Self {
+            window,
+            mouse_offset,
+        }
+    }
+
+    fn at_mouse_location(system_wide_element: &AXUIElement) -> Option<Self> {
+        let mouse_location = get_mouse_location().unwrap();
+        let window = Window::at_point(system_wide_element, &mouse_location);
 
         window
             .map(|window| {
-                let window_pos: CGPoint = window.position().unwrap().get_value().unwrap();
+                let window_pos: CGPoint = window.get_position().unwrap();
                 let mouse_offset = CGPoint::new(
                     mouse_location.x - window_pos.x,
                     mouse_location.y - window_pos.y,
@@ -99,37 +140,11 @@ impl WindowState {
             .ok()
     }
 
-    fn position(&self, x: f64, y: f64) {
-        let position = CGPoint::new(x, y);
-
-        self.window
-            .set_attribute(
-                &AXAttribute::position(),
-                AXValue::from_CGPoint(position).unwrap(),
-            )
-            .unwrap();
-    }
-
-    fn position_around(&self, point: &CGPoint) {
+    fn set_position_around(&self, point: &CGPoint) -> Result<(), accessibility::Error> {
         let x = point.x - self.mouse_offset.x;
         let y = point.y - self.mouse_offset.y;
 
-        self.position(x, y);
-    }
-
-    fn resize(&self, w: f64, h: f64) {
-        let size = CGSize::new(w, h);
-        self.window
-            .set_attribute(&AXAttribute::size(), AXValue::from_CGSize(size).unwrap())
-            .unwrap();
-    }
-
-    fn activate(&self) {
-        get_application(&self.window)
-            .unwrap()
-            .set_attribute(&AXAttribute::frontmost(), true)
-            .unwrap();
-        self.window.set_main(true).unwrap();
+        self.window.set_position(x, y)
     }
 }
 
@@ -179,17 +194,19 @@ fn main() {
                 match event_type {
                     MouseMoved => {
                         if let Some(state) = state.borrow().window_state.as_ref() {
-                            state.position_around(&event.location())
+                            state.set_position_around(&event.location()).unwrap()
                         }
                         CGEventTapCallbackResult::Keep
                     }
 
                     FlagsChanged => {
                         let mut s = state.borrow_mut();
-                        if event.get_flags().contains(CGEventFlags::CGEventFlagCommand) && s.mode == Mode::Normal {
-                            s.window_state = WindowState::from_mouse_location(&system_wide_element);
+                        if event.get_flags().contains(CGEventFlags::CGEventFlagCommand)
+                            && s.mode == Mode::Normal
+                        {
+                            s.window_state = WindowState::at_mouse_location(&system_wide_element);
                             if let Some(window_state) = s.window_state.as_ref() {
-                                window_state.activate()
+                                window_state.window.activate().unwrap()
                             }
                         } else if event
                             .get_flags()
@@ -213,32 +230,32 @@ fn main() {
                         match (&state.borrow().mode, keycode) {
                             (Mode::Normal, 4) => {
                                 // h
-                                let ws = WindowState::from_mouse_location(&system_wide_element);
+                                let ws = WindowState::at_mouse_location(&system_wide_element);
                                 if let Some(window_state) = ws.as_ref() {
-                                    window_state.activate();
-                                    window_state.position(0., 0.);
-                                    window_state.resize(w as f64 / 2., h as f64);
+                                    window_state.window.activate().unwrap() ;
+                                    window_state.window.set_position(0.,0.).unwrap();
+                                    window_state.window.set_size(w as f64 / 2., h as f64).unwrap();
                                 }
                                 CGEventTapCallbackResult::Drop
                             }
 
                             (Mode::Normal, 36) => {
                                 // <ENTER>
-                                let ws = WindowState::from_mouse_location(&system_wide_element);
+                                let ws = WindowState::at_mouse_location(&system_wide_element);
                                 if let Some(window_state) = ws.as_ref() {
-                                    window_state.activate();
-                                    window_state.position(0., 0.);
-                                    window_state.resize(w as f64, h as f64);
+                                    window_state.window.activate().unwrap() ;
+                                    window_state.window.set_position(0.,0.).unwrap();
+                                    window_state.window.set_size(w as f64, h as f64).unwrap();
                                 }
                                 CGEventTapCallbackResult::Drop
                             }
                             (Mode::Normal, 37) => {
                                 // l
-                                let ws = WindowState::from_mouse_location(&system_wide_element);
+                                let ws = WindowState::at_mouse_location(&system_wide_element);
                                 if let Some(window_state) = ws.as_ref() {
-                                    window_state.activate();
-                                    window_state.position(w as f64 / 2., 0.);
-                                    window_state.resize(w as f64 / 2., h as f64);
+                                    window_state.window.activate().unwrap() ;
+                                    window_state.window.set_position(w as f64 / 2., 0.).unwrap();
+                                    window_state.window.set_size(w as f64 / 2., h as f64).unwrap();
                                 }
                                 CGEventTapCallbackResult::Drop
                             }
