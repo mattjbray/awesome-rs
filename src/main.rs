14d15d95a3f3b949d3700a1d31117c8e225760e6
Matt Bray <mattjbray@gmail.com>
2024-03-16 00:14:48 +0000

    refactor: extract callback fn

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ use core_graphics::{
         CGDisplay,
     },
     event::{
-        CGEventFlags, CGEventTap, CGEventTapCallbackResult, CGEventTapLocation, CGEventTapOptions,
-        CGEventTapPlacement, CGEventType, EventField,
+        CGEvent, CGEventFlags, CGEventTap, CGEventTapCallbackResult, CGEventTapLocation,
+        CGEventTapOptions, CGEventTapPlacement, CGEventType, EventField,
     },
     window::{kCGWindowLayer, kCGWindowOwnerPID},
 };
@@ -80,89 +80,7 @@ fn main() {
             CGEventTapPlacement::HeadInsertEventTap,
             CGEventTapOptions::Default,
             vec![MouseMoved, FlagsChanged, KeyDown],
-            |_, event_type, event| {
-                match event_type {
-                    MouseMoved => {
-                        if let Some(dw) = state.borrow().drag_window() {
-                            dw.set_position_around(&event.location()).unwrap()
-                        }
-                        CGEventTapCallbackResult::Keep
-                    }
-
-                    FlagsChanged => {
-                        // println!("FlagsChanged {:?}", event.get_flags());
-                        let mut s = state.borrow_mut();
-                        if event.get_flags().contains(awesome_normal_mode_flags()) {
-                            s.toggle_mode();
-                        } else if event
-                            .get_flags()
-                            .contains(awesome_normal_mode_drag_window_flags())
-                            && s.is_normal_mode()
-                        {
-                            let ws = DragWindow::at_mouse_location().unwrap_or_else(|e| {
-                                eprintln!("While getting window at mouse location: {}", e);
-                                None
-                            });
-                            s.set_drag_window(ws);
-                            // if let Some(window_state) = s.window_state.as_ref() {
-                            //     window_state.window.activate().unwrap()
-                            // }
-                        } else {
-                            s.set_drag_window(None);
-                        }
-                        CGEventTapCallbackResult::Keep
-                    }
-                    KeyDown => {
-                        let keycode =
-                            event.get_integer_value_field(EventField::KEYBOARD_EVENT_KEYCODE);
-                        println!("KeyDown {}", keycode);
-                        let mut s = state.borrow_mut();
-                        match keycode {
-                            AWESOME_NORMAL_MODE_WINDOW_FULL_KEY if s.is_normal_mode() => {
-                                s.set_active_window_full().unwrap_or_else(|e| {
-                                    eprintln!("While setting window full: {}", e)
-                                });
-                                CGEventTapCallbackResult::Drop
-                            }
-
-                            AWESOME_NORMAL_MODE_WINDOW_LEFT_KEY if s.is_normal_mode() => {
-                                s.set_active_window_left().unwrap_or_else(|e| {
-                                    eprintln!("While setting window left: {}", e)
-                                });
-                                CGEventTapCallbackResult::Drop
-                            }
-
-                            AWESOME_NORMAL_MODE_WINDOW_RIGHT_KEY if s.is_normal_mode() => {
-                                s.set_active_window_right().unwrap_or_else(|e| {
-                                    eprintln!("While setting window right: {}", e)
-                                });
-                                CGEventTapCallbackResult::Drop
-                            }
-
-                            AWESOME_NORMAL_MODE_NEXT_WINDOW_KEY if s.is_normal_mode() => {
-                                s.next_window().unwrap_or_else(|e| {
-                                    eprintln!("While switching to next window: {}", e)
-                                });
-                                CGEventTapCallbackResult::Drop
-                            }
-
-                            AWESOME_NORMAL_MODE_PREV_WINDOW_KEY if s.is_normal_mode() => {
-                                s.prev_window().unwrap_or_else(|e| {
-                                    eprintln!("While switching to prev window: {}", e)
-                                });
-                                CGEventTapCallbackResult::Drop
-                            }
-
-                            _ => {
-                                // Enter Insert mode on any other key
-                                s.exit_normal_mode();
-                                CGEventTapCallbackResult::Keep
-                            }
-                        }
-                    }
-                    _ => CGEventTapCallbackResult::Keep,
-                }
-            },
+            mk_event_tap_callback(&state),
         )
         .unwrap()
     };
@@ -180,4 +98,87 @@ fn main() {
     );
 
     CFRunLoop::run_current();
+}
+
+fn mk_event_tap_callback(
+    state: &RefCell<WindowManager>,
+) -> impl Fn(*const c_void, CGEventType, &CGEvent) -> CGEventTapCallbackResult + '_ {
+    use CGEventType::*;
+    |_, event_type, event| {
+        match event_type {
+            MouseMoved => {
+                if let Some(dw) = state.borrow().drag_window() {
+                    dw.set_position_around(&event.location()).unwrap()
+                }
+                CGEventTapCallbackResult::Keep
+            }
+
+            FlagsChanged => {
+                // println!("FlagsChanged {:?}", event.get_flags());
+                let mut s = state.borrow_mut();
+                if event.get_flags().contains(awesome_normal_mode_flags()) {
+                    s.toggle_mode();
+                } else if event
+                    .get_flags()
+                    .contains(awesome_normal_mode_drag_window_flags())
+                    && s.is_normal_mode()
+                {
+                    let ws = DragWindow::at_mouse_location().unwrap_or_else(|e| {
+                        eprintln!("While getting window at mouse location: {}", e);
+                        None
+                    });
+                    s.set_drag_window(ws);
+                    // if let Some(window_state) = s.window_state.as_ref() {
+                    //     window_state.window.activate().unwrap()
+                    // }
+                } else {
+                    s.set_drag_window(None);
+                }
+                CGEventTapCallbackResult::Keep
+            }
+            KeyDown => {
+                let keycode = event.get_integer_value_field(EventField::KEYBOARD_EVENT_KEYCODE);
+                println!("KeyDown {}", keycode);
+                let mut s = state.borrow_mut();
+                match keycode {
+                    AWESOME_NORMAL_MODE_WINDOW_FULL_KEY if s.is_normal_mode() => {
+                        s.set_active_window_full()
+                            .unwrap_or_else(|e| eprintln!("While setting window full: {}", e));
+                        CGEventTapCallbackResult::Drop
+                    }
+
+                    AWESOME_NORMAL_MODE_WINDOW_LEFT_KEY if s.is_normal_mode() => {
+                        s.set_active_window_left()
+                            .unwrap_or_else(|e| eprintln!("While setting window left: {}", e));
+                        CGEventTapCallbackResult::Drop
+                    }
+
+                    AWESOME_NORMAL_MODE_WINDOW_RIGHT_KEY if s.is_normal_mode() => {
+                        s.set_active_window_right()
+                            .unwrap_or_else(|e| eprintln!("While setting window right: {}", e));
+                        CGEventTapCallbackResult::Drop
+                    }
+
+                    AWESOME_NORMAL_MODE_NEXT_WINDOW_KEY if s.is_normal_mode() => {
+                        s.next_window()
+                            .unwrap_or_else(|e| eprintln!("While switching to next window: {}", e));
+                        CGEventTapCallbackResult::Drop
+                    }
+
+                    AWESOME_NORMAL_MODE_PREV_WINDOW_KEY if s.is_normal_mode() => {
+                        s.prev_window()
+                            .unwrap_or_else(|e| eprintln!("While switching to prev window: {}", e));
+                        CGEventTapCallbackResult::Drop
+                    }
+
+                    _ => {
+                        // Enter Insert mode on any other key
+                        s.exit_normal_mode();
+                        CGEventTapCallbackResult::Keep
+                    }
+                }
+            }
+            _ => CGEventTapCallbackResult::Keep,
+        }
+    }
 }
