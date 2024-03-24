7ba267432c7aa763912c224eb5902316857d4978
Matt Bray <mattjbray@gmail.com>
2024-03-24 18:42:08 +0000

    refactor: flags

--- src/action.rs
+++ src/action.rs
@@ -32,53 +32,50 @@ const KEYCODE_L: i64 = 37;
 const KEYCODE_R: i64 = 15;
 const KEYCODE_T: i64 = 17;
 const KEYCODE_ENT: i64 = 36;
-const FLG_CTL: CGEventFlags = CGEventFlags::CGEventFlagControl;
+const FLG_NULL: CGEventFlags = CGEventFlags::CGEventFlagNull;
+const FLG_CTRL: CGEventFlags = CGEventFlags::CGEventFlagControl;
 const FLG_ALT: CGEventFlags = CGEventFlags::CGEventFlagAlternate;
+const FLG_SHIFT: CGEventFlags = CGEventFlags::CGEventFlagShift;
+const FLG_CMD: CGEventFlags = CGEventFlags::CGEventFlagCommand;
 
 impl Action {
     pub fn of_cg_event(event: &CGEvent, mode: &Mode, layout: &Layout) -> Option<Self> {
         match event.get_type() {
             CGEventType::KeyDown => {
-                let flags = event.get_flags();
+                // Extract only relevant flags so we can use (==)
+                let flags = event
+                    .get_flags()
+                    .intersection(FLG_CTRL | FLG_ALT | FLG_SHIFT | FLG_CMD);
                 let keycode = event.get_integer_value_field(EventField::KEYBOARD_EVENT_KEYCODE);
                 println!("KeyDown ({:?}) {}", mode, keycode);
-                match (mode, keycode, layout) {
-                    // CTRL + ALT + A
-                    (Mode::Insert, KEYCODE_A, _) if flags.contains(FLG_CTL | FLG_ALT) => {
+                match (mode, flags, keycode, layout) {
+                    (Mode::Insert, _, KEYCODE_A, _) if flags == FLG_CTRL | FLG_ALT => {
                         Some(Action::ModeNormal)
                     }
-                    (Mode::Normal, KEYCODE_A, _) => Some(Action::LayoutCascade),
-                    (Mode::Normal, KEYCODE_F, _) => Some(Action::LayoutFloating),
-                    (Mode::Normal, KEYCODE_H, Layout::TileHorizontal(_))
-                        if flags.contains(FLG_ALT) =>
-                    {
+                    (Mode::Normal, FLG_NULL, KEYCODE_A, _) => Some(Action::LayoutCascade),
+                    (Mode::Normal, FLG_NULL, KEYCODE_F, _) => Some(Action::LayoutFloating),
+                    (Mode::Normal, FLG_ALT, KEYCODE_H, Layout::TileHorizontal(_)) => {
                         Some(Action::DecrPrimaryColWindows)
                     }
-                    (Mode::Normal, KEYCODE_L, Layout::TileHorizontal(_))
-                        if flags.contains(FLG_ALT) =>
-                    {
+                    (Mode::Normal, FLG_ALT, KEYCODE_L, Layout::TileHorizontal(_)) => {
                         Some(Action::IncrPrimaryColWindows)
                     }
-                    (Mode::Normal, KEYCODE_H, Layout::TileHorizontal(_)) => {
+                    (Mode::Normal, FLG_NULL, KEYCODE_H, Layout::TileHorizontal(_)) => {
                         Some(Action::DecrPrimaryColWidth)
                     }
-                    (Mode::Normal, KEYCODE_L, Layout::TileHorizontal(_)) => {
+                    (Mode::Normal, FLG_NULL, KEYCODE_L, Layout::TileHorizontal(_)) => {
                         Some(Action::IncrPrimaryColWidth)
                     }
-                    (Mode::Normal, KEYCODE_H, _) => Some(Action::WindowLeftHalf),
-                    (Mode::Normal, KEYCODE_L, _) => Some(Action::WindowRightHalf),
-                    (Mode::Normal, KEYCODE_R, _) => Some(Action::RefreshWindowList),
-                    (Mode::Normal, KEYCODE_T, _) => Some(Action::LayoutTiling),
-                    (Mode::Normal, KEYCODE_J, _) if flags.contains(FLG_ALT) => {
-                        Some(Action::SwapNextWindow)
-                    }
-                    (Mode::Normal, KEYCODE_K, _) if flags.contains(FLG_ALT) => {
-                        Some(Action::SwapPrevWindow)
-                    }
-                    (Mode::Normal, KEYCODE_J, _) => Some(Action::NextWindow),
-                    (Mode::Normal, KEYCODE_K, _) => Some(Action::PrevWindow),
-                    (Mode::Normal, KEYCODE_ENT, _) => Some(Action::WindowFull),
-                    (Mode::Normal, _, _) => Some(Action::ModeInsert),
+                    (Mode::Normal, FLG_NULL, KEYCODE_H, _) => Some(Action::WindowLeftHalf),
+                    (Mode::Normal, FLG_NULL, KEYCODE_L, _) => Some(Action::WindowRightHalf),
+                    (Mode::Normal, FLG_NULL, KEYCODE_R, _) => Some(Action::RefreshWindowList),
+                    (Mode::Normal, FLG_NULL, KEYCODE_T, _) => Some(Action::LayoutTiling),
+                    (Mode::Normal, FLG_ALT, KEYCODE_J, _) => Some(Action::SwapNextWindow),
+                    (Mode::Normal, FLG_ALT, KEYCODE_K, _) => Some(Action::SwapPrevWindow),
+                    (Mode::Normal, FLG_NULL, KEYCODE_J, _) => Some(Action::NextWindow),
+                    (Mode::Normal, FLG_NULL, KEYCODE_K, _) => Some(Action::PrevWindow),
+                    (Mode::Normal, FLG_NULL, KEYCODE_ENT, _) => Some(Action::WindowFull),
+                    (Mode::Normal, _, _, _) => Some(Action::ModeInsert),
                     _ => None,
                 }
             }
